ca70efb3a19dba69543a71417d02fac811929ba9
Dhastry <139798946+Dhastry@users.noreply.github.com>
2023-10-06 15:11:29 -0600

    Update and rename MainP.java to Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class MainP {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
