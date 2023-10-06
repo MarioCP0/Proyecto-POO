65550f36a6f612611701d2e27534c6e921a49876
Dhastry <139798946+Dhastry@users.noreply.github.com>
2023-10-06 15:18:18 -0600

    Add files via upload

---
+++ Tema.java
@@ -0,0 +1,38 @@
+class Tema {
+    private String nombre;
+    private String informacion;
+    private Prueba prueba;
+
+    public Tema(String nombre, String informacion) {
+        this.nombre = nombre;
+        this.informacion = informacion;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getInformacion() {
+        return this.informacion;
+    }
+
+    public void setInformacion(String informacion) {
+        this.informacion = informacion;
+    }
+
+    public Prueba getPrueba() {
+        return this.prueba;
+    }
+
+    public void setPrueba(Prueba prueba) {
+        this.prueba = prueba;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println(this.informacion);
+    }
+}
---
+++ prueba.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Prueba {
+    private List<String> preguntas;
+    private List<String> respuestasCorrectas;
+
+    public Prueba(List<String> preguntas, List<String> respuestasCorrectas) {
+        this.preguntas = preguntas;
+        this.respuestasCorrectas = respuestasCorrectas;
+    }
+
+    public List<String> getPreguntas() {
+        return this.preguntas;
+    }
+
+    public void setPreguntas(List<String> preguntas) {
+        this.preguntas = preguntas;
+    }
+
+    public List<String> getRespuestasCorrectas() {
+        return this.respuestasCorrectas;
+    }
+
+    public void setRespuestasCorrectas(List<String> respuestasCorrectas) {
+        this.respuestasCorrectas = respuestasCorrectas;
+    }
+
+    public void iniciarPrueba() {
+        Scanner scanner = new Scanner(System.in);
+        int score = 0;
+    
+        for (int i = 0; i < preguntas.size(); i++) {
+            System.out.println(preguntas.get(i));
+            String respuestaUsuario = scanner.nextLine();
+    
+            if (respuestaUsuario.equalsIgnoreCase(respuestasCorrectas.get(i))) {
+                score++;
+            }
+        }
+    
+        System.out.println("Tu puntuación es: " + score + "/" + preguntas.size());
+    }
+    
+    }
+
