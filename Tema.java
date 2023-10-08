4df9a563de4b92066fe569698a5a502ad71e6c65
isaramirezapa <140284940+isaramirezapa@users.noreply.github.com>
2023-10-07 19:54:34 -0600

    Comentarios
    Comentarios javadoc con @param y @return con carátula

--- Tema.java
+++ Tema.java
@@ -1,37 +1,75 @@
+//UNIVERSIDAD DEL VALLE DE GUATEMALA
+//PROGRAMACIÓN ORIENTADA A OBJETOS
+// DAHSRTY SECAIRA, MARIO CABRERA, ISABELLA RAMÍREZ, ESTEBAN RAMÍREZ
+/**
+ * La clase Tema representa un tema de aprendizaje con un nombre, información y una prueba asociada.
+ */
 class Tema {
     private String nombre;
     private String informacion;
     private Prueba prueba;
 
+    /**
+     * Constructor para crear un nuevo objeto Tema con un nombre y una descripción.
+     * @param nombre El nombre del tema.
+     * @param informacion La descripción o información sobre el tema.
+     */
     public Tema(String nombre, String informacion) {
         this.nombre = nombre;
         this.informacion = informacion;
     }
 
+    /**
+     * Obtiene el nombre del tema.
+     * @return El nombre del tema.
+     */
     public String getNombre() {
         return this.nombre;
     }
 
+    /**
+     * Establece el nombre del tema.
+     * @param nombre El nuevo nombre para el tema.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Obtiene la información del tema.
+     * @return La información o descripción del tema.
+     */
     public String getInformacion() {
         return this.informacion;
     }
 
+    /**
+     * Establece la información del tema.
+     * @param informacion La nueva información o descripción del tema.
+     */
     public void setInformacion(String informacion) {
         this.informacion = informacion;
     }
 
+    /**
+     * Obtiene la prueba asociada al tema.
+     * @return La prueba asociada al tema.
+     */
     public Prueba getPrueba() {
         return this.prueba;
     }
 
+    /**
+     * Establece la prueba asociada al tema.
+     * @param prueba La nueva prueba asociada al tema.
+     */
     public void setPrueba(Prueba prueba) {
         this.prueba = prueba;
     }
 
+    /**
+     * Muestra la información o descripción del tema en la consola.
+     */
     public void mostrarInformacion() {
         System.out.println(this.informacion);
     }
--- main.java
+++ main.java
@@ -1,7 +1,13 @@
+//UNIVERSIDAD DEL VALLE DE GUATEMALA
+//PROGRAMACIÓN ORIENTADA A OBJETOS
+// DAHSRTY SECAIRA, MARIO CABRERA, ISABELLA RAMÍREZ, ESTEBAN RAMÍREZ
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Esta es la clase principal que contiene el método principal `main`.
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
