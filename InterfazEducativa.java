f5b6d4aa18c59211cb23004b2c15fe2616d480ae
Estebancin <esteban1110rp@gmail.com>
2023-11-09 20:18:49 -0600

    Mejoras de interfaz
    La interfaz ya permite navegar entre preguntas.

--- InterfazEducativa.java
+++ InterfazEducativa.java
@@ -1,11 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class InterfazEducativa {
@@ -18,13 +18,13 @@ public class InterfazEducativa {
     private JRadioButton opcion2;
     private JRadioButton opcion3;
     private JRadioButton opcion4;
-    private JButton botonSiguiente;
+    private int respuestasCorrectas = 0;
+    private int respuestasIncorrectas = 0;
     private List<String[]> preguntas;
-    private int indicePreguntaActual = 0;
-    private String nivelDificultad;
+    private int preguntaActual = 0; // Índice de la pregunta actual
 
     public InterfazEducativa(String nombreUsuario, String nivelDificultad) {
-        this.nivelDificultad = nivelDificultad;
+        // Cargar las preguntas desde el archivo CSV
         preguntas = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("preguntas.csv"))) {
             String linea;
@@ -35,6 +35,8 @@ public class InterfazEducativa {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Collections.shuffle(preguntas);
+
 
         // Crear el marco de la ventana
         frame = new JFrame("AstroEducación");
@@ -59,62 +61,98 @@ public class InterfazEducativa {
         // Crear los botones de opción para las respuestas
         grupoOpciones = new ButtonGroup();
         opcion1 = new JRadioButton("Opción 1");
+        opcion1.setActionCommand("Opción 1"); // Configura el ActionCommand
         opcion1.setBounds(50, 80, 300, 25);
         grupoOpciones.add(opcion1);
         panel.add(opcion1);
 
         opcion2 = new JRadioButton("Opción 2");
+        opcion2.setActionCommand("Opción 2"); // Configura el ActionCommand
         opcion2.setBounds(50, 110, 300, 25);
         grupoOpciones.add(opcion2);
         panel.add(opcion2);
 
         opcion3 = new JRadioButton("Opción 3");
+        opcion3.setActionCommand("Opción 3"); // Configura el ActionCommand
         opcion3.setBounds(50, 140, 300, 25);
         grupoOpciones.add(opcion3);
         panel.add(opcion3);
 
         opcion4 = new JRadioButton("Opción 4");
+        opcion4.setActionCommand("Opción 4"); // Configura el ActionCommand
         opcion4.setBounds(50, 170, 300, 25);
         grupoOpciones.add(opcion4);
         panel.add(opcion4);
 
-        // Crear el botón de "siguiente"
-        botonSiguiente = new JButton("Siguiente");
-        botonSiguiente.setBounds(50, 200, 300, 25);
-        botonSiguiente.addActionListener(new ActionListener() {
+        // Crear un botón para enviar la respuesta
+        JButton botonResponder = new JButton("Responder");
+        botonResponder.setBounds(50, 200, 100, 25);
+        panel.add(botonResponder);
+
+        // Agregar un ActionListener al botón para manejar la respuesta del usuario
+        botonResponder.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                indicePreguntaActual++;
-                if (indicePreguntaActual < preguntas.size() && indicePreguntaActual < 3) {
-                    actualizarPregunta();
-                } else {
-                    // Aquí puedes redirigir a la pantalla final
-                    JOptionPane.showMessageDialog(frame, "¡Prueba terminada! Mostraremos tu calificación pronto.");
-                }
+                verificarRespuesta();
             }
         });
-        panel.add(botonSiguiente);
 
-        // Buscar la primera pregunta para el nivel de dificultad seleccionado
-        actualizarPregunta();
+        // Mostrar la primera pregunta del nivel de dificultad seleccionado
+        mostrarSiguientePregunta();
 
         // Hacer visible la ventana
         frame.setVisible(true);
     }
 
-    private void actualizarPregunta() {
-        for (int i = indicePreguntaActual; i < preguntas.size(); i++) {
-            String[] pregunta = preguntas.get(i);
-            if (pregunta[0].equals(nivelDificultad)) {
-                etiquetaPregunta.setText(pregunta[2]);
-                // Aquí puedes actualizar las opciones de respuesta
-                opcion1.setText(pregunta[3]);
-                opcion2.setText(pregunta[4]);
-                opcion3.setText(pregunta[5]);
-                opcion4.setText(pregunta[6]);
-                break;  // Esto hará que solo se muestre la primera pregunta que coincida con el nivel de dificultad
+    private void verificarRespuesta() {
+    String[] pregunta = preguntas.get(preguntaActual);
+    String respuestaCorrecta = pregunta[7]; // La respuesta correcta se encuentra en el campo 7
+
+    // Obtener el modelo del botón seleccionado por el usuario
+    ButtonModel model = grupoOpciones.getSelection();
+    
+    if (model instanceof DefaultButtonModel) {
+        // Obtener el botón de radio del modelo
+        JRadioButton respuestaSeleccionada = (JRadioButton) ((DefaultButtonModel) model).getGroup().getElements().nextElement();
+
+        if (respuestaSeleccionada != null) {
+            String respuestaUsuario = respuestaSeleccionada.getText();
+            if (respuestaUsuario.equals(respuestaCorrecta)) {
+                respuestasCorrectas++; // Incrementa el contador de respuestas correctas
+                JOptionPane.showMessageDialog(frame, "¡Respuesta correcta!", "Feedback", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                respuestasIncorrectas++; // Incrementa el contador de respuestas incorrectas
+                JOptionPane.showMessageDialog(frame, "Respuesta incorrecta. La respuesta correcta es: " + respuestaCorrecta, "Feedback", JOptionPane.ERROR_MESSAGE);
             }
+        } else {
+            JOptionPane.showMessageDialog(frame, "Por favor, seleccione una respuesta.", "Feedback", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+    
+    // Mostrar la siguiente pregunta
+    mostrarSiguientePregunta();
+}
+    
+    // Mostrar puntuacion
+    private void mostrarPuntuacion() {
+        JOptionPane.showMessageDialog(frame, "Puntuación final:\nRespuestas correctas: " + respuestasCorrectas + "\nRespuestas incorrectas: " + respuestasIncorrectas, "Puntuación", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+
+    private void mostrarSiguientePregunta() {
+        if (preguntaActual < preguntas.size() - 1 && preguntaActual<4) {
+            preguntaActual++;
+            String[] pregunta = preguntas.get(preguntaActual);
+            etiquetaPregunta.setText(pregunta[2]);
+            opcion1.setText(pregunta[3]);
+            opcion2.setText(pregunta[4]);
+            opcion3.setText(pregunta[5]);
+            opcion4.setText(pregunta[6]);
+            grupoOpciones.clearSelection(); // Limpiar la selección de respuestas
+        } else {
+            mostrarPuntuacion();
+            JOptionPane.showMessageDialog(frame, "Has completado todas las preguntas.", "Fin del cuestionario", JOptionPane.INFORMATION_MESSAGE);
+            frame.dispose(); // Cierra la ventana después de completar todas las preguntas
         }
     }
 }
-
