27cdbd3fb42d33806043afca510857e489a84d06
Dhastry <dhastrysecaira2016@gmail.com>
2023-11-12 11:50:28 -0600

    Update VentanaInformacion.java

--- VentanaInformacion.java
+++ VentanaInformacion.java
@@ -4,42 +4,40 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
+/**
+ * Esta clase representa una ventana de información.
+ * La ventana muestra el contenido de un archivo de texto y tiene un botón para cerrar la ventana.
+ */
+
 public class VentanaInformacion extends JFrame {
     public VentanaInformacion() {
-        // Configuracion de ventana
         setTitle("Información Importante");
         setSize(1920, 1080);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Crea un panel
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        // Agrega el texto
         JLabel labelInformacion = new JLabel();
         labelInformacion.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(labelInformacion);
         
-        // Agrega un botón para cerrar la ventana
         JButton botonContinuar = new JButton("Continuar");
         botonContinuar.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.add(botonContinuar);
 
-        // Agrega un ActionListener para cerrar la ventana
         botonContinuar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                dispose(); // Cierra la ventana de información
+                dispose(); 
             }
         });
 
-        // Agrega el panel a la ventana
         add(panel);
 
-        // Centra la ventana en la pantalla
         setLocationRelativeTo(null);
 
-        // visible la ventana de información
         setVisible(true);
 
         String rutaArchivo = "informacion.txt";
@@ -47,21 +45,19 @@ public class VentanaInformacion extends JFrame {
         try (BufferedReader br = new BufferedReader(new FileReader(rutaArchivo))) {
             StringBuilder contenidoArchivo = new StringBuilder();
             String linea;
-            // Lee cada línea del archivo txt
             while ((linea = br.readLine()) != null) {
-                // Agrega cada línea al contenido del archivo
                 contenidoArchivo.append(linea).append("<br>");
             }
-            // Establece el contenido de la interfaz con el contenido del archivo
             labelInformacion.setText("<html>" + contenidoArchivo.toString() + "</html>");
         } catch (IOException e) {
-        // si el archivo no se encuentra lo ignora
             e.printStackTrace();
         }
     }
-
+    /**
+     * Método principal que crea una instancia de la clase VentanaInformacion.
+     * @param args Argumentos de la línea de comandos.
+     */
     public static void main(String[] args) {
-        // Crea una instancia de la ventana de información
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
